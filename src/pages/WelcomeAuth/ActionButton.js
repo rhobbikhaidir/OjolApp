8cbe145a9fa47232ba7192a19f975e2e19668a5e
rhobbikhaidir <rhobbie09@gmail.com>
2020-08-27 09:30:25 +0700

    saya merapikan component-component seperti button di pisah agat lebuh dinamis dan menurut saya , di materi ini cukup menyulitkan tetap fokus Rhobbi Khaidir

--- src/pages/WelcomeAuth/ActionButton.js
+++ src/pages/WelcomeAuth/ActionButton.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Button } from '../../component';
+import { colors } from '../utils';
 
 
 const ActionButton = ({desc, title}) => {
     return(
-        <View style={{marginBottom: 43, 
-        maxWidth: 225}}>
-            <Text style={{fontSize: 10, 
-                color:"#7e7e7e", 
-                textAlign: 'center', 
-                paddingHorizontal: '15%', 
-                marginBottom: 6 }}>{desc}</Text>
-            <TouchableOpacity style={{fontSize: 10, 
-                backgroundColor: "#a55eea", 
-                borderRadius: 25, 
-                paddingVertical: 13}}>
-                <Text style={{fontSize: 12, 
-                    fontWeight: 'bold', 
-                    color: 'white', 
-                    textTransform: "uppercase", 
-                    textAlign: 'center'}}>{title}</Text>
-            </TouchableOpacity>
+        <View style={styles.wrapper.component}>
+            <Text style={styles.text.desc}>{desc}</Text>
+            <Button title={title} />
         </View>
     )
 }
 
+
+const styles= {
+    wrapper: {
+        component: {
+            marginBottom: 43, 
+            maxWidth: 225
+        }
+    },
+    text: {
+        desc: {
+            fontSize: 10, 
+            color:colors.text.default, 
+            textAlign: 'center', 
+            paddingHorizontal: '15%', 
+            marginBottom: 6
+        }
+    }
+}
 
 
 
--- src/pages/WelcomeAuth/index.js
+++ src/pages/WelcomeAuth/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import ActionButton from './ActionButton'
+import { colors } from '../utils';
 
 const WelcomeAuth = () => {
     return(
-        <View style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'white',
-            flex: 1
-        }}>
-            <View style={{width: 219, 
-                height: 117, 
-                backgroundColor: '#A55eea', 
-                marginBottom: 10}}/>
-            <Text style={{fontSize: 18, 
-                fontWeight: 'bold', 
-                color: '#A55eea', 
-                marginBottom: 76}}>Selamat Datang di O-JOL</Text>
+        <View style={styles.wrapper.page}>
+            <View style={styles.wrapper.ilustrasi}/>
+            <Text style={styles.text.welcome}>Selamat Datang di O-JOL</Text>
             <ActionButton  desc="Silahkan Masuk, jika anda sudah memiliki akun"
             title="Masuk"/>
             <ActionButton desc="atau silahkan daftar jika anda belum memiliki akun" title="Daftar"/>
@@ -25,6 +15,32 @@ const WelcomeAuth = () => {
     )
 }
 
+
+
+const styles=  {
+    wrapper: {
+        page: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            flex: 1,
+        },
+        ilustrasi: {
+            width: 219, 
+            height: 117, 
+            backgroundColor: colors.default, 
+            marginBottom: 10
+        }
+    },
+    text: {
+        welcome: {
+            fontSize: 18, 
+            fontWeight: 'bold', 
+            color: colors.default, 
+            marginBottom: 76
+        }
+    }
+}
 
 
 export default WelcomeAuth;
